7370a2e3a792c7744ab27a8a937f7077fbd1aa25
teoavas <matteosavastano2@gmail.com>
2026-03-20 00:34:53 +0100

    aggiornato main.py

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 app = FastAPI()
 
-@app.get("/hello")
+@app.get("/items/pippo")
+def hello_world_pippo():
+    return f"Hello World! - ENDPOINT pippo"
+@app.get("/items/{item_id}")
+def hello_world(item_id: str):
+    return f"Hello World! item_id={item_id}"
 
-def hello_world():
-    return "Hello World!"
+#def hello_world(item_id:int , param1:int, param2:int=2):
+#    return f"Hello World! item_id={item_id}, param1: {param1}, param2: {param2}"
 
